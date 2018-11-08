671c706173846ddda4cdfa6b11dd64d6d056c3e2
TaeJuneJoung <43630534+TaeJuneJoung@users.noreply.github.com>
2018-11-08 23:51:05 +0900

    2018.11.8

--- Java/src/studyInterface/StudyInterface.java
+++ Java/src/studyInterface/StudyInterface.java
@@ -33,8 +33,131 @@ package studyInterface;
  * 구현 클래스
  * public class 구현클래스명 implements 인터페이스명 { 인터페이스에 선언된 추상 메소드의 실체 메소드 선언 }
  * 
- * 
+ * 인터페이스에서 인터페이스를 상속 받을 때는 extends를 사용하고 2개 이상 사용 가능
+ * 클래스를 상속 받을 때는 2개 이상 불가지만, 인터페이스를 상속 받을 때는 2개 이상 가능
  * */
 public class StudyInterface {
 
+	/* 상위 1: 동물
+	 * 상위 2: 육식 / 초식
+	 * 상위3: 사자,늑대/소,말 
+	 * */
+	
+	public static void main(String[] args) {
+		
+		Animal animal = new Lion();
+		animal.sound();
+		animal.walk(true);
+		animal.walk(!true);
+		
+		Carnivores carnivores = new Lion();
+		carnivores.sound();
+		
+	}//end of main
+}//end of class
+
+interface Animal {
+	//상수
+	public static final int MIN_NUMBER = 0;
+	public static final int MAX_NUMBER = 10;
+	//추상 메소드
+	public abstract void sound();
+	
+	//디폴트 메소드
+	public default void walk(boolean isWalk) {
+		if(isWalk) {
+			System.out.println("활보합니다.");
+		}else {
+			sweeping();
+			System.out.println("우리에 가둡니다.");
+		}
+	}
+	//정적 메소드
+//	public static void buy() {
+//		//정적 메소드는 오버라이드 하여 재정의 할 수 없다. <-buy요소로 사용하면 맞지 않음
+//	}
+	public static void sweeping() {//정적 메소드는 인터페이스 내에서 사용함(?) <- 사용법을 정확히 모르겠음
+		System.out.println("가두기 전에 우리를 청소합니다.");
+	}
+	
+	//디폴트 메소드와 정적 메소드 사용법에 대해서 아직 잘 이해하지 못함
+	//추상 메소드를 통해 재정의를 해서 이용하면 되는거 아닌지. 필요성을 굳이 못 느낌
+	
+}
+
+class Herbivores implements Animal {//herbivores - 초식동물
+
+	@Override
+	public void sound() {
+		System.out.print("초식동물_ ");
+	}
+}
+
+class Carnivores implements Animal {//carnivores - 육식동물
+
+	@Override
+	public void sound() {
+		System.out.print("육식동물 _ ");
+	}
+}
+
+class Lion extends Carnivores {
+	private int number = 0;
+	private final String NAME = "Lion";
+	@Override
+	public void sound() {
+		System.out.println("크ㅡ헝");
+	}
+	@Override
+	public void walk(boolean isWalk) {
+		super.walk(isWalk);
+	}
+}
+
+class Wolf extends Carnivores {
+	private int number = 0;
+	private final String NAME = "Wolf";
+	@Override
+	public void sound() {
+		System.out.println("아ㅡ울");
+	}
+	@Override
+	public void walk(boolean isWalk) {
+		if(isWalk)
+			System.out.println(this.NAME+"이 산책을 시작합니다.\n"+this.NAME+"이 기분이 좋아집니다.");
+		else
+			System.out.println(this.NAME+"을 우리에 가둡니다.");
+	}
+}
+
+class Bull extends Herbivores {
+	private int number = 0;
+	private final String NAME = "Bull";
+	@Override
+	public void sound() {
+		System.out.println("모ㅡ오");
+	}
+	@Override
+	public void walk(boolean isWalk) {
+		if(isWalk)
+			System.out.println(this.NAME+"이 산책을 시작합니다.\n"+this.NAME+"이 기분이 좋아집니다.");
+		else
+			System.out.println(this.NAME+"을 우리에 가둡니다.");
+	}
+}
+
+class Horse extends Herbivores {
+	private int number = 0;
+	private final String NAME = "Horse";
+	@Override
+	public void sound() {
+		System.out.println("히ㅡ잉");
+	}
+	@Override
+	public void walk(boolean isWalk) {
+		if(isWalk)
+			System.out.println(this.NAME+"이 산책을 시작합니다.\n"+this.NAME+"이 기분이 좋아집니다.");
+		else
+			System.out.println(this.NAME+"을 우리에 가둡니다.");
+	}
 }
---
+++ Java/src/studyNestedClass/StudyNestedClass.java
@@ -0,0 +1,37 @@
+package studyNestedClass;
+
+/* 중첩 클래스(Nested Class)를 왜 사용하는가?
+ * -> 어떤 클래스는 여러 클래스와 관계를 맺지만 -> 독립적 선언
+ * 어떤 클래스는 특정 클래스와 관계를 맺는다. -> 클래스 내부 선언(중첩 클래스)
+ * 
+ * #선언 위치에 따른 구분
+ *  1. 멤버 클래스
+ *  	- 인스턴스 멤버 클래스 : A객체를 생성해야만 사용할 수 있는 B중첩 클래스
+ *  		class A{
+ *  			class B{...}
+ *  		}
+ *  	- 정적 멤버 클래스 : A클래스로 바로 접근할 수 있는 B중첩 클래스
+ *  		class A{
+ *  			static class B{...}
+ *  		}
+ *  2. 로컬 클래스 : method()가 실행할 때만 사용할 수 있는 B중첩 클래스
+ *  	: 메소드 안에 쓰여진 중첩 클래스
+ *  		class A{
+ *  			void method(){
+ *  				class B{...}
+ *  			}//end of method
+ *  		}//end of class
+ * */
+public class StudyNestedClass {
+	public class NestedClassEx{//중첩 클래스의 경우 public일 때 public 사용 가능
+	//중첩 클래스를 사용하면 두 클래스의 멤버들을 서로 쉽게 접근할 수 있다는 장점과
+	//외부에는 불필요한 관계 클래스를 감춤으로써 코드의 복잡성을 줄일 수 있다
+	}
+	
+	public interface NestedInterfaceEx{
+	//해당 클래스와 긴밀한 관계를 맺는 구현 클래스를 만들기 위해 인터페이스를 내부에 선언
+	//중첩 인터페이스는 주로 UI프로그래밍에서 이벤트를 처리할 목적으로 많이 활용
+	//	ex) 안드로이드에서 View클래스 내부에 OnClickListener
+	}
+	
+}//end of class
